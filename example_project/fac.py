ca540fd5ecb0ebddf8c3fedeb936128d12dbdc5d
Lumir Balhar <lbalhar@redhat.com>
2018-01-25 09:01:49 +0100

    Move example project to its own repository and use it as submodule

--- example_project/fac.py
+++
@@ -1,8 +0,0 @@
-def fac(n):
-    if not isinstance(n, int) or n < 0:
-        raise ValueError('n must be non-negative integer')
-
-    if n == 0:
-        return 1
-    else:
-        return n * fac(n - 1)
