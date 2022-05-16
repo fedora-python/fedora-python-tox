0710b2f4c76daa70de428ff6bcf29ea325a33664
Aurélien Bompard <aurelien@bompard.org>
2022-05-16 18:51:26 +0200

    Mark the repo as safe for Git
    This relates to https://github.com/actions/checkout/issues/760
    
    Some tests need git to be functional (eg pre-commit checks) and since
    CVE-2022-24765 git checks directory ownership.
    
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- run_tests.sh
+++ run_tests.sh
@@ -13,4 +13,7 @@ if [ ! -z "$DNF_INSTALL" ]; then
     dnf -y --setopt=tsflags=nodocs --setopt=deltarpm=false install $DNF_INSTALL
 fi
 
+# Mark the current directory as safe for Git:
+git config --global --add safe.directory `pwd`
+
 /usr/bin/tox $TOX_PARAMS
